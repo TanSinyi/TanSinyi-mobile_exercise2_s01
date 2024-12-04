a2d424c501401ddf083eca719b65ad45d0b64f90
Tan Sin Yi <sinyitan0806@gmail.com>
2024-12-04 12:58:40 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MAP Exercise 2 - TESTING',
+      title: 'MAP Exercise 2',
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
