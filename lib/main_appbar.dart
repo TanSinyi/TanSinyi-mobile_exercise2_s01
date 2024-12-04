308e96fdbe82fe7b0d32cf31658220deb8f97642
Tan Sin Yi <sinyitan0806@gmail.com>
2024-12-04 18:15:51 +0800

    Modify the comments  driver: Tan Sin Yi navigator: Goh Zi Qin

--- lib/main_appbar.dart
+++ lib/main_appbar.dart
@@ -2,105 +2,125 @@ import 'package:flutter/material.dart';
 
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
-  Size get preferredSize => Size.fromHeight(60.0);
-  final Function onDecrease;
-  final Function onIncrease;
-  final Function smallSize;
-  final Function mediumSize;
-  final Function largeSize;
-  final bool allowResize;
+  Size get preferredSize => const Size.fromHeight(60.0);
 
-  MainAppBar({
-    required this.onDecrease,
-    required this.onIncrease,
-    required this.smallSize,
-    required this.mediumSize,
-    required this.largeSize,
-    required this.allowResize,
-  });
+  final Function onDecrease; // Function for the minus button
+  final Function onIncrease; // Function for the plus button
+  final Function smallSize; // Function for the S button
+  final Function mediumSize; // Function for the M button
+  final Function largeSize; // Function for the L button
+  final bool allowResize; // Declare allowResize variable
+  
+  MainAppBar(
+      {required this.onDecrease,
+      required this.smallSize,
+      required this.mediumSize,
+      required this.largeSize,
+      required this.onIncrease,
+      required this.allowResize});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.brown,
       leading: IconButton(
-          icon: const Icon(Icons.menu),
-          color: Colors.white,
-          onPressed: () {
-            Scaffold.of(context).openDrawer();
-          }),
+        icon: const Icon(Icons.menu),
+        color: Colors.white,
+        onPressed: () {
+          // Open the drawer when the menu icon is pressed
+          Scaffold.of(context).openDrawer();
+        },
+      ),
       title: const Text('My Icon',
-          style: TextStyle(fontSize: 25.0, color: Colors.white)),
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.white,
+          )),
       actions: [
+        // Display resize buttons only if allowResize is true
         if (allowResize) ...[
-          //minimize button
           IconButton(
-              onPressed: () => onDecrease(),
-              icon: const Icon(
-                Icons.remove_circle_outline,
-                color: Colors.white,
-              )),
-          //'S'button
+            onPressed: () => onDecrease(),
+            icon: const Icon(
+              Icons
+                  .remove_circle_outline, // Icon for remove with circle outline
+              color: Colors.white, // Icon color
+            ),
+          ),
           TextButton(
-              onPressed: () => smallSize(),
-              child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2.0,
-                      )),
-                  padding: const EdgeInsets.all(5.0),
-                  child: const Text(
-                    'S',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold),
-                  ))),
-          //'M' Button
+            onPressed: () => smallSize(),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle, // Circle shape
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2.0, // Border width
+                ),
+              ),
+              padding: const EdgeInsets.all(
+                  5.0), // Padding inside the circle to ensure text doesn't touch the border
+              child: const Text(
+                'S', // The text shown on the button
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.0, // Font size
+                  fontWeight: FontWeight.bold, // Font weight
+                ),
+              ),
+            ),
+          ),
           TextButton(
-              onPressed: () => mediumSize(),
-              child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2.0,
-                      )),
-                  padding: const EdgeInsets.all(5.0),
-                  child: const Text(
-                    'M',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold),
-                  ))),
-          //'L'Button
+            onPressed: () => mediumSize(),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle, // Circle shape
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2.0, // Border width
+                ),
+              ),
+              padding: const EdgeInsets.all(
+                  5.0), // Padding inside the circle to ensure text doesn't touch the border
+              child: const Text(
+                'M', // The text shown on the button
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.0, // Font size
+                  fontWeight: FontWeight.bold, // Font weight
+                ),
+              ),
+            ),
+          ),
           TextButton(
-              onPressed: () => largeSize(),
-              child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2.0,
-                      )),
-                  padding: const EdgeInsets.all(5.0),
-                  child: const Text(
-                    'L',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.bold),
-                  ))),
-          //plus button
+            onPressed: () => largeSize(),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle, // Circle shape
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2.0, // Border width
+                ),
+              ),
+              padding: const EdgeInsets.all(
+                  5.0), // Padding inside the circle to ensure text doesn't touch the border
+              child: const Text(
+                'L', // The text shown on the button
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.0, // Font size
+                  fontWeight: FontWeight.bold, // Font weight
+                ),
+              ),
+            ),
+          ),
           IconButton(
-              onPressed: () => onIncrease(),
-              icon: const Icon(
-                Icons.add_circle_outline,
-                color: Colors.white,
-              )),
+            onPressed: () =>
+                onIncrease(), // Define what happens when the button is pressed
+            icon: const Icon(
+              Icons.add_circle_outline, // Icon for remove with circle outline
+              color: Colors.white,
+            ),
+          ),
         ]
       ],
     );
--- lib/main_bottom_bar.dart
+++ lib/main_bottom_bar.dart
@@ -23,98 +23,109 @@ class MainBottomBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 180.0,
-      child: Column(children: [
-        //Red Slider and Button/Text
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (allowChangeColor) ...[
-              Expanded(
-                child: Slider(
-                  value: redSliderValue,
-                  min: 0,
-                  max: 255,
-                  onChanged: onRedSliderChanged,
-                  activeColor: Colors.brown,
-                  inactiveColor: Colors.brown.withOpacity(0.3),
+      child: Column(
+        children: [
+          // Red Slider and Button/Text
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (allowChangeColor) ...[
+                Expanded(
+                  child: Slider(
+                    value: redSliderValue,
+                    min: 0,
+                    max: 255,
+                    onChanged: onRedSliderChanged,
+                    activeColor: Colors.brown, // Set the slider color to brown
+                    inactiveColor:
+                        Colors.brown.withOpacity(0.3), // Dim the inactive part
+                  ),
                 ),
-              ),
-              FloatingActionButton(
-                backgroundColor: Colors.red,
-                shape: const CircleBorder(),
-                onPressed: () => onRedSliderChanged(255),
-                child: Text(
-                  redSliderValue.toStringAsFixed(0),
-                  style: const TextStyle(color: Colors.white),
+                FloatingActionButton(
+                  backgroundColor: Colors.red, // Use red color for background
+                  shape: const CircleBorder(),
+                  onPressed: () => onRedSliderChanged(255),
+                  child: Text(
+                    redSliderValue.toStringAsFixed(0),
+                    style: const TextStyle(color: Colors.white),
+                  ),
                 ),
-              )
-            ] else ...[
-              Text('Value for R(Red): ${redSliderValue.toStringAsFixed(0)}',
-                  style: const TextStyle(fontSize: 20, color: Colors.red)),
-            ]
-          ],
-        ),
-        //Green Slider and Button/Text
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (allowChangeColor) ...[
-              Expanded(
-                child: Slider(
-                  value: greenSliderValue,
-                  min: 0,
-                  max: 255,
-                  onChanged: onGreenSliderChanged,
-                  activeColor: Colors.brown,
-                  inactiveColor: Colors.brown.withOpacity(0.3),
+              ] else ...[
+                Text(
+                  ('Value for R (Red): ${redSliderValue.toStringAsFixed(0)}'),
+                  style: const TextStyle(fontSize: 20, color: Colors.red),
                 ),
-              ),
-              FloatingActionButton(
-                backgroundColor: Colors.green,
-                shape: const CircleBorder(),
-                onPressed: () => onGreenSliderChanged(255),
-                child: Text(
-                  greenSliderValue.toStringAsFixed(0),
-                  style: const TextStyle(color: Colors.white),
+              ],
+            ],
+          ),
+          // Green Slider and Button/Text
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (allowChangeColor) ...[
+                Expanded(
+                  child: Slider(
+                    value: greenSliderValue,
+                    min: 0,
+                    max: 255,
+                    onChanged: onGreenSliderChanged,
+                    activeColor: Colors.brown, // Set the slider color to brown
+                    inactiveColor:
+                        Colors.brown.withOpacity(0.3), // Dim the inactive part
+                  ),
                 ),
-              )
-            ] else ...[
-              Text('Value for G(Green): ${greenSliderValue.toStringAsFixed(0)}',
-                  style: const TextStyle(fontSize: 20, color: Colors.green)),
-            ]
-          ],
-        ),
-        //Blue Slider and Button/Text
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (allowChangeColor) ...[
-              Expanded(
-                child: Slider(
-                  value: blueSliderValue,
-                  min: 0,
-                  max: 255,
-                  onChanged: onBlueSliderChanged,
-                  activeColor: Colors.brown,
-                  inactiveColor: Colors.brown.withOpacity(0.3),
+                FloatingActionButton(
+                  backgroundColor: Colors.green,
+                  shape: const CircleBorder(),
+                  onPressed: () => onGreenSliderChanged(255),
+                  child: Text(
+                    greenSliderValue.toStringAsFixed(0),
+                    style: const TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
-              FloatingActionButton(
-                backgroundColor: Colors.blue,
-                shape: const CircleBorder(),
-                onPressed: () => onBlueSliderChanged(255),
-                child: Text(
-                  blueSliderValue.toStringAsFixed(0),
-                  style: const TextStyle(color: Colors.white),
+              ] else ...[
+                Text(
+                  ('Value for G (Green): ${greenSliderValue.toStringAsFixed(0)}'),
+                  style: const TextStyle(fontSize: 20, color: Colors.green),
                 ),
-              )
-            ] else ...[
-              Text('Value for B(Blue): ${blueSliderValue.toStringAsFixed(0)}',
-                  style: const TextStyle(fontSize: 20, color: Colors.blue)),
-            ]
-          ],
-        ),
-      ]),
+              ],
+            ],
+          ),
+          // Blue Slider and Button/Text
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (allowChangeColor) ...[
+                Expanded(
+                  child: Slider(
+                    value: blueSliderValue,
+                    min: 0,
+                    max: 255,
+                    onChanged: onBlueSliderChanged,
+                    activeColor: Colors.brown, // Set the slider color to brown
+                    inactiveColor:
+                        Colors.brown.withOpacity(0.3), // Dim the inactive part
+                  ),
+                ),
+                FloatingActionButton(
+                  backgroundColor: Colors.blue,
+                  shape: const CircleBorder(),
+                  onPressed: () => onBlueSliderChanged(255),
+                  child: Text(
+                    blueSliderValue.toStringAsFixed(0),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ] else ...[
+                Text(
+                  ('Value for B (Blue): ${blueSliderValue.toStringAsFixed(0)}'),
+                  style: const TextStyle(fontSize: 20, color: Colors.blue),
+                ),
+              ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
